9218f060402530205aae0b9a1cd9c261cf9d43de
vanshitalavange <86543154+vanshitalavange@users.noreply.github.com>
2021-10-06 16:52:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import React, { useState } from "react";
+import stocks from "./images/stocks.svg";
+
+export default function App() {
+  return <div className="App"></div>;
+}
